6cdc702dfac1e232d54499bb23f774c07060c3e5
Miron Kalenta <miron_kalenta@epam.com>
2024-01-27 12:25:45 +0100

    added sanitizing

--- validators/commonValidators.js
+++ validators/commonValidators.js
@@ -1,11 +1,11 @@
 const { body } = require('express-validator');
 
 const validateName = () => {
-  return body('name').isString().withMessage('Name must be a string');
+  return body('name').isString().withMessage('Name must be a string').trim().escape();
 };
 
 const validateAge = () => {
-  return body('age').isInt({ min: 0 }).withMessage('Age must be a positive integer');
+  return body('age').isInt({ min: 0 }).withMessage('Age must be a positive integer').toInt();
 };
 
 module.exports = {
